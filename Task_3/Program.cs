348065d59427497492c9f1672d96f21d01f33181
Serhii <serhii.soldatov@gmail.com>
2022-04-22 20:19:36 +0200

    Task_3 completed

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+System.Console.WriteLine("Введите цифру, отвечающую дню недели: ");
+int day;
+day = int.Parse(Console.ReadLine());
+if (day == 1)
+    Console.WriteLine("Monday, go to work");
+if (day == 2)
+    Console.WriteLine("Tuesday, go to work");
+if (day == 3)
+    Console.WriteLine("Wednesday, go to work");
+if (day == 4)
+    Console.WriteLine("Thursday, go to work");
+if (day == 5)
+    Console.WriteLine("Friday, go to work");
+if (day == 6)
+    Console.WriteLine("Saturday, day off!");
+if (day == 7)
+    Console.WriteLine("Saturday, day off!");  
